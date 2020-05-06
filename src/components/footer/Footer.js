aafa078df4243b581c4d8795f749bca3975bdd04
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-06 08:55:08 +0300

    update footer css

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
           <li>
             <a
               className="footer-div-1-a"
-              href="https://www.youtube.com/"
+              href="https://www.youtube.com/channel/UChkxm4Q14X0NdGFvZOm1hMQ"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -81,34 +81,34 @@ function Footer() {
           <li>
             <a
               className="footer-div-2-a"
-              href="https://www.linkedin.com/in/t%C3%BCrkay-tun%C3%A7-674353177/"
+              href="https://twitter.com/filthycoder"
               target="_blank"
               rel="noopener noreferrer"
             >
               <p>
                 <img
-                  src={`${process.env.PUBLIC_URL}/footer/linkedin.png`}
+                  src={`${process.env.PUBLIC_URL}/footer/twitterlogo.jpg`}
                   height="16px"
-                  alt="linkedin-img"
+                  alt="twitter-img"
                 />
-                LinkedIn
+                Twitter
               </p>
             </a>
           </li>
           <li>
             <a
               className="footer-div-2-a"
-              href="https://www.youtube.com/"
+              href="https://turkaytunc.itch.io/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <p>
                 <img
-                  src={`${process.env.PUBLIC_URL}/footer/youtube.png`}
+                  src={`${process.env.PUBLIC_URL}/footer/itchiologo.png`}
                   height="16px"
-                  alt="youtube-img"
+                  alt="itchio-img"
                 />
-                YouTube
+                Itch.io
               </p>
             </a>
           </li>
