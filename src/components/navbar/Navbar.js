ef45886e57f62110675aa198a4b383ace707d6ed
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-04 18:59:37 +0300

    added divs for hamburger menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,30 +4,37 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar-div">
-      <ul className="navbar-ul">
-        <li className="navbar-li">
-          <Link className="navbar-a" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="navbar-li">
-          <Link className="navbar-a" to="/projects">
-            Projects
-          </Link>
-        </li>
-        <li className="navbar-li">
-          <Link className="navbar-a" to="/articles">
-            Articles
-          </Link>
-        </li>
-        <li className="navbar-li">
-          <Link className="navbar-a" to="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar-div">
+        <ul className="navbar-ul">
+          <li className="navbar-li">
+            <Link className="navbar-a" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="navbar-li">
+            <Link className="navbar-a" to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className="navbar-li">
+            <Link className="navbar-a" to="/articles">
+              Articles
+            </Link>
+          </li>
+          <li className="navbar-li">
+            <Link className="navbar-a" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="nav-hamburger">
+        <div className="hamburger-bar1"></div>
+        <div className="hamburger-bar2"></div>
+        <div className="hamburger-bar3"></div>
+      </div>
+    </div>
   );
 }
 
