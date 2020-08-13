5228b5909b868a40d1874dcb6a3ebf9726dc3c08
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-13 14:52:29 +0300

    update

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,25 +2,15 @@ import React from "react";
 import "./homepage.styles.scss";
 
 function HomePage() {
-  const randColor = `rgb(
-    ${~~(Math.random() * 255)},
-    ${~~(Math.random() * 255)},
-    ${~~(Math.random() * 255)}
-  )`;
-
-  console.log(randColor);
   return (
     <div className="homepage-wrapper">
-      <p className="intro" style={{ color: `${randColor}` }}>
-        Hi, I'm Türkay TUNÇ.
+      <p className="intro">Hi, I'm Türkay TUNÇ.</p>
+      <p className="intro">
+        Front-end Web Developer. <br />
+        Javascript lover. <br />
+        Likes to do weird things with React and Sass.
       </p>
-      <p className="intro" style={{ color: `${randColor}` }}>
-        I'm a front-end web developer. <br />
-        Doing front-end design with React and Sass.
-      </p>
-      <p className="intro" style={{ color: `${randColor}` }}>
-        As a hobby making games and tools with Unity and C#.
-      </p>
+      <p className="intro">Making games and tools with Unity and C#.</p>
     </div>
   );
 }
