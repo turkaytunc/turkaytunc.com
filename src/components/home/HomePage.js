23d050c0cc2c59df7cf2e7d3babbde69ad4050eb
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-16 22:52:32 +0300

    deploy

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,10 +4,12 @@ import './homepage.styles.scss';
 function HomePage() {
   return (
     <div className="homepage-wrapper">
-      <p className="intro">Türkay TUNÇ.</p>
+      <p className="intro">Türkay TUNÇ</p>
       <p className="intro">
         Front-end Web Developer. <br />
+        <br />
         Javascript lover. <br />
+        <br />
         Likes to do weird things with React and Sass.
       </p>
       <p className="intro">Makes games and creates tools with Unity and C#.</p>
